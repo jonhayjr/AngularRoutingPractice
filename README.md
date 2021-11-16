28ec1f122783782d0813d228bd95beb85ca84240
jonhayjr <jonhayjr@gmail.com>
2021-11-16 07:57:01 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ydxjrt-s4hhf8
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ydxjrt-s4hhf8)
---
+++ src/app/routes.ts
@@ -0,0 +1,10 @@
+import { ActorListComponent } from './actor-list/actor-list.component';
+import { ActorEditComponent } from './actor-edit.component';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component'
+
+export const routingTable = [
+  {path: '', redirectTo: 'actors', pathMatch: 'full'},
+  {path: 'actors', component: ActorListComponent},
+  {path: 'actors/:id/edit', component: ActorEditComponent},
+  {path: '**', component: FourOhFourComponent}
+]
